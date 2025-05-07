0e5863f0006d73a459b69fd053efdf45625e9d2c
vtedash <rastavte@gmail.com>
2025-05-07 21:57:16 +0200

    EMPEZAMOS DE CERO CON MEJOR INICIO

--- my-turn/DIARIO.gd
+++
@@ -1,5 +0,0 @@
-###Esto es un diario de desarrollo para saber por donde voy.
-
-## VOY POR AQUI Escena de Batalla (BattleScene.tscn) Escena: Nueva escena, nodo raíz Node2D. Nombre: BattleScene.
-
-###TESTINGCAMBIOS
